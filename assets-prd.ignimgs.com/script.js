1ff1520bfae6819d1ed0faf6ce9c7ac97d8b5a0a
Pedro <kyussvt@hotmail.com>
2024-07-20 04:49:52 -0300

    whatever

--- assets-prd.ignimgs.com/script.js
+++ assets-prd.ignimgs.com/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log('Document loaded, starting script...');
     
-    fetch('assets-prd.ignimgs.com/images.json')
+    fetch('images.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
@@ -14,16 +14,11 @@ document.addEventListener("DOMContentLoaded", function() {
             if (images.length > 0) {
                 const randomIndex = Math.floor(Math.random() * images.length);
                 const randomImage = images[randomIndex];
-                const baseUrl = 'https://kyussvt.github.io/assets-prd.ignimgs.com/images/';
-                const imageUrl = `${baseUrl}${randomImage}`;
+                const imageUrl = `https://kyussvt.github.io/assets-prd.ignimgs.com/images/${randomImage}`;
                 console.log('Random image URL:', imageUrl);
-
-                // Change the image src
+                
                 const randomImageElement = document.getElementById('randomImage');
                 randomImageElement.src = imageUrl;
-
-                // Change the URL without reloading the page
-                history.pushState(null, '', imageUrl);
             } else {
                 console.error("No images found in the JSON file.");
             }
